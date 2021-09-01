9111ea8065e3cb0db2c37bb785a4f0153359c2df
unknown <73797608+Chickegg@users.noreply.github.com>
2021-09-01 23:18:44 +0900

    React-create-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import Main from './pages/Main';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Main from "./pages/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// import 나의 메인 페이지 Component 불러오기
+
+/// Component들이 모이는 App.js
 
 function App() {
   return (
-    <div>
-      <Main></Main>
-    </div>
+    <Router>
+      <Header />
+      <Route path="/">
+        <Main />
+      </Route>
+      <Footer />
+    </Router>
   );
 }
 
