5a06a25cab6c053e945c15f1f4669043fcc9814f
unknown <73797608+Chickegg@users.noreply.github.com>
2021-09-01 23:26:09 +0900

    add:Footer, Header

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import "./footer.css"
+const Footer = () => {
+    return (
+        <div className="footer-wrapper">
+            <div className="home-button">홈</div>
+            <div className="search-button">검색</div>
+            <div className="category-button">카테고리</div>
+            <div className="login-button">로그인</div>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+const Header = (props) => {
+    return (
+        <div className="header-wrapper">Mini Market</div>
+    )
+}
+
+
+
+export default Header;
